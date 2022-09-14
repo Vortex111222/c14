476cc735418700659ad2043e4bc86c119b9791ae
Vortex111222 <111537241+Vortex111222@users.noreply.github.com>
2022-09-14 16:42:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var student= {
+  name:'bhangra', 
+  class:2,
+  roll_number:4,
+  marks: [93,98,87] 
+}
+console.log(student.name)
+console.log(student.class)
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
